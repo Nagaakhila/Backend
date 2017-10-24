434e5504903e41cb450426e4a5777a8e4bd1cefd
NAGA AKHILA <NAGA AKHILA@LAPTOP-C1KQN4GL>
2017-10-24 15:05:43 +0530

    colloboration Backend

---
+++ src/test/java/com/collaborate/Backend/BlogDAOTestCase.java
@@ -0,0 +1,45 @@
+package com.collaborate.Backend;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.collaborate.DAO.BlogDao;
+import com.collaborate.Model.Blog;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class BlogDAOTestCase {
+
+	static BlogDao blogDAO;
+	@BeforeClass
+	public static void initialize()
+	{
+		AnnotationConfigApplicationContext annotationConfigAppContext=new AnnotationConfigApplicationContext();
+		annotationConfigAppContext.scan("com.collaborate");
+		annotationConfigAppContext.refresh();
+		blogDAO=(BlogDao)annotationConfigAppContext.getBean("blogDAO");
+	}
+	
+@Test
+	
+	public void createBlogTest()
+	{
+		
+	Blog blog=new Blog();	
+	
+	blog.setBlogId(1000);
+	blog.setBlogName("Sai Mahesh");
+	blog.setBlogContent("Sai Is Thop");
+	blog.setUsername("sai.mahesh246@gmail.com");
+	blog.setStatus("NA");
+	blog.setCreateDate(new java.util.Date());
+	blog.setLikes(0);
+	
+	assertTrue("problem in blog Creation",blogDAO.createBlog(blog));
+	}
+	
+	
+}
